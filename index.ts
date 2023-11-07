aae33b24ade2006e2393a41a38ef5398dd2c201c
Ming-der Wang <mingderwang@gmail.com>
2023-11-08 00:09:38 +0800

    pimlico-tutorial-1

--- index.ts
+++ index.ts
@@ -6,4 +6,49 @@ import { Address, Hash, concat, createClient, createPublicClient, encodeFunction
 import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
 import { lineaTestnet, polygonMumbai } from "viem/chains"
 
-console.log("Hello world!")
+dotenv.config();
+const apiKey=process.env.PIMLICO_API_KEY
+console.log(apiKey);
+
+// CREATE THE CLIENTS
+const publicClient = createPublicClient({
+  transport: http("https://rpc.goerli.linea.build/"),
+  chain: lineaTestnet
+})
+ 
+const chain = "linea-testnet" // find the list of chain names on the Pimlico verifying paymaster reference page
+ 
+const bundlerClient = createClient({
+  transport: http(`https://api.pimlico.io/v1/${chain}/rpc?apikey=${apiKey}`),
+  chain: lineaTestnet
+}).extend(bundlerActions).extend(pimlicoBundlerActions)
+ 
+const paymasterClient = createClient({
+  // ⚠️ using v2 of the API ⚠️ 
+  transport: http(`https://api.pimlico.io/v2/${chain}/rpc?apikey=${apiKey}`),
+  chain: lineaTestnet
+}).extend(pimlicoPaymasterActions)
+
+// GENERATE THE INITCODE
+const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454"
+ 
+const ownerPrivateKey = generatePrivateKey()
+const owner = privateKeyToAccount(ownerPrivateKey)
+ 
+console.log("Generated wallet with private key:", ownerPrivateKey)
+ 
+const initCode = concat([
+  SIMPLE_ACCOUNT_FACTORY_ADDRESS,
+  encodeFunctionData({
+    abi: [{
+      inputs: [{ name: "owner", type: "address" }, { name: "salt", type: "uint256" }],
+      name: "createAccount",
+      outputs: [{ name: "ret", type: "address" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    }],
+    args: [owner.address, 0n]
+  })
+]);
+ 
+console.log("Generated initCode:", initCode)
